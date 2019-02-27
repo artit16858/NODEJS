93c796520a1aa1e8b6a5628e41678ab0dbe6f1ec
warayutpetch <k00000ko@hotmail.com>
2019-02-27 16:24:27 +0700

    +++

---
+++ desktop/Appusabai.bat
@@ -0,0 +1,6 @@
+@echo oof
+timeout 1 
+cd "C:\Users\Revel Soft 03\Documents\GitHub\IONIC\USABAI"
+ionic cordova run android -lc -- -d
+
+:exit
---
+++ desktop/runnotificationusabai.bat
@@ -0,0 +1,6 @@
+@echo oof
+timeout 2 
+cd "C:\Users\Revel Soft 03\Documents\GitHub\NODEJS"
+node group.js
+
+:exit
